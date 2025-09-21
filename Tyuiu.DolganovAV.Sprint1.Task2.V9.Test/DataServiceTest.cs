7325a68b560ef961f0452b5e4bb6116a5bdb8aef
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-09-21 18:53:11 +0500

    Task2

---
+++ Tyuiu.DolganovAV.Sprint1.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.DolganovAV.Sprint1.Task2.V9.Lib;
+namespace Tyuiu.DolganovAV.Sprint1.Task2.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int r = 5;
+            var res = ds.CalculateVolumeCircle(r);
+            Assert.AreEqual(523.599, res);
+        }
+    }
+}
---
+++ Tyuiu.DolganovAV.Sprint1.Task2.V9/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.DolganovAV.Sprint1.Task2.V9.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Долганов А. В. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #25                                                             *");
+        Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет вычисление объема шара и печатает результат на экране.        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int r;
+        Console.WriteLine("Введите значение радиуса шара:");
+        r = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.CalculateVolumeCircle(r));
+        Console.ReadLine();
+    }
+}
