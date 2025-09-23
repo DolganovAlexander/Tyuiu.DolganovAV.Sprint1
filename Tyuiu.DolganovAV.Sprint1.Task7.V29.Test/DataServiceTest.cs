870de078167292b295ab5197217c43fa6e8db60a
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-09-23 21:39:45 +0500

    Task7

---
+++ Tyuiu.DolganovAV.Sprint1.Task7.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.DolganovAV.Sprint1.Task7.V29.Lib;
+
+namespace Tyuiu.DolganovAV.Sprint1.Task7.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 5.0;
+            double y = 4.0;
+            Assert.AreEqual(4.985, ds.Calculate(x, y));
+        }
+    }
+}
---
+++ Tyuiu.DolganovAV.Sprint1.Task7.V29/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.DolganovAV.Sprint1.Task7.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Долганов А. В. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+        Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("                    3              5    ");
+        Console.WriteLine("               cos x          sin x     ");
+        Console.WriteLine(" z = x  -  -----------  +  -----------  ");
+        Console.WriteLine("             x * y - 3      x * y + 5   ");
+        Console.WriteLine("");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите числа: ");
+        Console.Write("x = ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y = ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine(ds.Calculate(x, y));
+        Console.ReadLine();
+    }
+}
