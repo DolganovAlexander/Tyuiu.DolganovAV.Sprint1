d1d0713bc0f9c515dfb27af99b8e72144961b814
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-09-23 14:38:52 +0500

    Task5

---
+++ Tyuiu.DolganovAV.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DolganovAV.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.DolganovAV.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 1.0;
+            double y1 = 4.0;
+            double x2 = 8.0;
+            double y2 = 8.0;
+            var res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            Assert.AreEqual(8, res);
+        }
+    }
+}
--- Tyuiu.DolganovAV.Sprint1.Task5.V1/Program.cs
+++ Tyuiu.DolganovAV.Sprint1.Task5.V1/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.DolganovAV.Sprint1.Task4.V1.Lib;
+﻿using Tyuiu.DolganovAV.Sprint1.Task5.V1.Lib;
 internal class Program
 {
     private static void Main(string[] args)
@@ -7,28 +7,36 @@ internal class Program
         Console.Title = "Спринт #1 | Выполнил: Долганов А. В. | ПИНб-25-1";
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #1                                                               *");
-        Console.WriteLine("* Тема: Class Math                                                        *");
-        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+        Console.WriteLine("* Задание #5                                                              *");
         Console.WriteLine("* Вариант #1                                                              *");
         Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
         Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-        Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+        Console.WriteLine("* вычисляет расстояние между двумя точками и печатает результат на экране *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        double number;
-        Console.WriteLine("Введите число: ");
-        number = Convert.ToDouble(Console.ReadLine());
+        double x1,x2,y1,y2;
+        Console.WriteLine("Введите координаты первой точки: ");
+        Console.Write("x1 = ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y1 = ");
+        y1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите координаты второй точки: ");
+        Console.Write("x2 = ");
+        x2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y2 = ");
+        y2 = Convert.ToDouble(Console.ReadLine());
 
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
 
-        Console.WriteLine(ds.Calculate(number));
+        Console.WriteLine(ds.DistanceBetweenDots(x1, y1, x2, y2));
         Console.ReadLine();
     }
 }
