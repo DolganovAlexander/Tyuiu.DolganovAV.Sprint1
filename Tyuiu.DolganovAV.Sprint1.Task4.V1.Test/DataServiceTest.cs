ebbd0596eb5e8244c79df7d300555460fc3afc59
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-09-23 14:12:14 +0500

    Task4

---
+++ Tyuiu.DolganovAV.Sprint1.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.DolganovAV.Sprint1.Task4.V1.Lib;
+namespace Tyuiu.DolganovAV.Sprint1.Task4.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 6.0;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(0.016, res);
+        }
+    }
+}
